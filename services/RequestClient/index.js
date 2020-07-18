d5ddd1335611e16a9427965763379340770b2959
Paul Taiwo <ayopaulot@gmail.com>
2020-07-18 18:52:26 +0100

    use data from api

--- services/RequestClient/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const RequestClient = axios.create({
-  baseURL: "https://stbc-api.herokuapp.com/api/v1",
-  timeout: 5000,
-});
-
-export default RequestClient;
--- src/pages/sermons/sermonList.js
+++ src/pages/sermons/sermonList.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import RequestClient from "../../services/RequestClient";
 import { Header, Footer } from "../../components";
+import SermonListCard from "../../components/sermonListCard";
 
 const SermonList = () => {
+  const [sermons, setSermons] = useState({
+    fetched: false,
+    data: [],
+  });
+
+  useEffect(() => {
+    RequestClient.get("sermons").then((res) => {
+      setSermons({ fetched: true, data: [...res.data.data] });
+    });
+  }, []);
+
   return (
     <>
       <div id='preloader'></div>
@@ -24,284 +38,15 @@ const SermonList = () => {
           <div className='container'>
             <div className='row'>
               <div className='col-md-10 col-md-offset-1'>
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(1).jpg'
-                      alt=''
-                      className='img-responsive'
+                {sermons.fetched &&
+                  sermons.data.length > 0 &&
+                  sermons.data.map(({ sermon_title, sermon_author, createdAt }) => (
+                    <SermonListCard
+                      sermonTitle={sermon_title}
+                      sermonAuthor={sermon_author}
+                      createdAt={createdAt}
                     />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Transforming Live, Restoring Hope</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(2).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Put Your Faith Into Action</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(3).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Transforming Live, Restoring Hope</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(4).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Transforming Live, Restoring Hope</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(5).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Put Your Faith Into Action</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(6).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Transforming Live, Restoring Hope</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(7).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Transforming Live, Restoring Hope</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(8).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Put Your Faith Into Action</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
-
-                <div className='custom-col-3'>
-                  <div className='left-col'>
-                    <img
-                      src='img/sermons/pic%20(9).jpg'
-                      alt=''
-                      className='img-responsive'
-                    />
-                  </div>
-                  <div className='mid-col'>
-                    <a href='#'>
-                      <h3>Transforming Live, Restoring Hope</h3>
-                    </a>
-                    <div className='details'>
-                      <span>
-                        By <a href='#'>John Rook</a>, October 12, 2014.
-                      </span>
-                    </div>
-                  </div>
-                  <div className='right-col'>
-                    <a href='#'>
-                      <i className='fa fa-video-camera'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-volume-up'></i>
-                    </a>
-                    <a href='#'>
-                      <i className='fa fa-file-pdf-o'></i>
-                    </a>
-                  </div>
-                </div>
+                  ))}
               </div>
             </div>
           </div>
