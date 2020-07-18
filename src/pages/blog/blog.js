7b91bb5404b85af26042919dae9e95441087fbbd
Paul Taiwo <ayopaulot@gmail.com>
2020-07-18 19:18:22 +0100

    use data from api

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import RequestClient from "../../services/RequestClient";
 import { Header, Footer } from "../../components";
 
 import blog1 from "../../assets/img/blog/pic-blog-1.jpg";
 import blog2 from "../../assets/img/blog/pic-blog-2.jpg";
 import blog3 from "../../assets/img/blog/pic-blog-3.jpg";
 import blog4 from "../../assets/img/blog/pic-blog-4.jpg";
+import BlogPostListCard from "../../components/blogPostListCard";
 
 const Blog = () => {
+  const [blogPost, setBlogPost] = useState({
+    fetched: false,
+    data: [],
+  });
+
+  useEffect(() => {
+    RequestClient.get("blogs").then((res) => {
+      console.log(res);
+      setBlogPost({ fetched: true, data: [...res.data.data] });
+    });
+  }, []);
+
   return (
     <>
       <div id='preloader'></div>
@@ -29,62 +44,27 @@ const Blog = () => {
             <div className='row'>
               <div className='col-md-8'>
                 <ul className='blog-list'>
-                  <li>
-                    <div className='info'>
-                      <div className='date-box'>
-                        <span className='day'>01</span>
-                        <span className='month'>FEB</span>
-                      </div>
-                    </div>
-                    <div className='preview'>
-                      <img src={blog1} alt='' />
-                      <a href='news-single.html'>
-                        <h3 className='blog-title'>
-                          Lin: 'God Is Challenging Me!'
-                        </h3>
-                      </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
-                    </div>
-                    <div className='meta-info'>
-                      By: <a href='#'>Admin</a>
-                      <span>|</span>
-                      <a href='#'>Faith</a>, <a href='#'>People</a>
-                      <span>|</span>
-                      <a href='#'>2 Comments</a>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div className='info'>
-                      <div className='date-box'>
-                        <span className='day'>05</span>
-                        <span className='month'>FEB</span>
-                      </div>
-                    </div>
-                    <div className='preview'>
-                      <img src={blog2} alt='' />
-                      <a href='news-single.html'>
-                        <h3 className='blog-title'>10 Bible Verses for 2017</h3>
-                      </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
-                    </div>
-                    <div className='meta-info'>
-                      By: <a href='#'>Admin</a>
-                      <span>|</span>
-                      <a href='#'>daily bread</a>, <a href='#'>inspiration</a>
-                      <span>|</span>
-                      <a href='#'>2 Comments</a>
-                    </div>
-                  </li>
-
+                  {blogPost.fetched &&
+                    blogPost.data.length > 0 &&
+                    blogPost.data.map(
+                      ({
+                        _id,
+                        blog_title,
+                        blog_author,
+                        featured_img,
+                        blog_content,
+                        createdAt,
+                      }) => (
+                        <BlogPostListCard
+                          key={_id}
+                          blogTitle={blog_title}
+                          blogAuthor={blog_author}
+                          featuredImg={featured_img}
+                          blogContent={blog_content}
+                          createdAt={createdAt}
+                        />
+                      )
+                    )}
                   <li>
                     <div className='info'>
                       <div className='date-box'>
@@ -97,11 +77,10 @@ const Blog = () => {
                       <a href='#'>
                         <h3 className='blog-title'>Crouch goes Home to Jesus</h3>
                       </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                      aliquip ex ea commodo consequat. Duis aute irure dolor in
                     </div>
                     <div className='meta-info'>
                       By: <a href='#'>Admin</a>
@@ -124,121 +103,10 @@ const Blog = () => {
                       <a href='#'>
                         <h3 className='blog-title'>Our Most Christian Congress?</h3>
                       </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
-                    </div>
-                    <div className='meta-info'>
-                      By: <a href='#'>Admin</a>
-                      <span>|</span>
-                      <a href='#'>Course</a>, <a href='#'>Promotion</a>
-                      <span>|</span>
-                      <a href='#'>2 Comments</a>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div className='info'>
-                      <div className='date-box'>
-                        <span className='day'>23</span>
-                        <span className='month'>FEB</span>
-                      </div>
-                    </div>
-                    <div className='preview'>
-                      <img src={blog2} alt='' />
-                      <a href='news-single.html'>
-                        <h3 className='blog-title'>Paris Hostage Crises Over</h3>
-                      </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
-                    </div>
-                    <div className='meta-info'>
-                      By: <a href='#'>Admin</a>
-                      <span>|</span>
-                      <a href='#'>Course</a>, <a href='#'>Promotion</a>
-                      <span>|</span>
-                      <a href='#'>2 Comments</a>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div className='info'>
-                      <div className='date-box'>
-                        <span className='day'>25</span>
-                        <span className='month'>FEB</span>
-                      </div>
-                    </div>
-                    <div className='preview'>
-                      <img src={blog1} alt='' />
-                      <a href='news-single.html'>
-                        <h3 className='blog-title'>
-                          Mitt Romney 'S Presidential Run
-                        </h3>
-                      </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
-                    </div>
-                    <div className='meta-info'>
-                      By: <a href='#'>Admin</a>
-                      <span>|</span>
-                      <a href='#'>Course</a>, <a href='#'>Promotion</a>
-                      <span>|</span>
-                      <a href='#'>2 Comments</a>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div className='info'>
-                      <div className='date-box'>
-                        <span className='day'>27</span>
-                        <span className='month'>FEB</span>
-                      </div>
-                    </div>
-                    <div className='preview'>
-                      <img src={blog4} alt='' />
-                      <a href='#'>
-                        <h3 className='blog-title'>5 Million Join Jesus Parade</h3>
-                      </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
-                    </div>
-                    <div className='meta-info'>
-                      By: <a href='#'>Admin</a>
-                      <span>|</span>
-                      <a href='#'>Course</a>, <a href='#'>Promotion</a>
-                      <span>|</span>
-                      <a href='#'>2 Comments</a>
-                    </div>
-                  </li>
-
-                  <li>
-                    <div className='info'>
-                      <div className='date-box'>
-                        <span className='day'>29</span>
-                        <span className='month'>FEB</span>
-                      </div>
-                    </div>
-                    <div className='preview'>
-                      <img src={blog3} alt='' />
-                      <a href='#'>
-                        <h3 className='blog-title'>Does Science Point to God?</h3>
-                      </a>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                      aliquip ex ea commodo consequat. Duis aute irure dolor in
                     </div>
                     <div className='meta-info'>
                       By: <a href='#'>Admin</a>
@@ -290,14 +158,12 @@ const Blog = () => {
                       </div>
                       <div className='txt'>
                         <h5>
-                          <a href='css/index.html#'>
-                            Lin: 'God Is Challenging Me!'
-                          </a>
+                          <a href='css/index.html#'>Lin: 'God Is Challenging Me!'</a>
                         </h5>
                         <div>
                           <div>
-                            Sed ut perspiciatis unde omnis iste natus error sit
-                            voluptatem accusantium doloremque{" "}
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque{" "}
                           </div>
                         </div>
                       </div>
@@ -311,8 +177,8 @@ const Blog = () => {
                         <h5>
                           <a href='css/index.html#'>10 Bible Verses for 2017</a>
                         </h5>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque
                       </div>
                     </li>
                     <li>
@@ -322,12 +188,10 @@ const Blog = () => {
                       </div>
                       <div className='txt'>
                         <h5>
-                          <a href='css/index.html#'>
-                            Andraé Crouch goes Home to Jesus
-                          </a>
+                          <a href='css/index.html#'>Andraé Crouch goes Home to Jesus</a>
                         </h5>
-                        Sed ut perspiciatis unde omnis iste natus error sit
-                        voluptatem accusantium doloremque
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque
                       </div>
                     </li>
                   </ul>
@@ -380,10 +244,10 @@ const Blog = () => {
                 <div className='widget widget-text'>
                   <h3>Our Address</h3>
                   <iframe
-                    frameborder='0'
+                    frameBorder='0'
                     scrolling='no'
-                    marginheight='0'
-                    marginwidth='0'
+                    marginHeight='0'
+                    marginWidth='0'
                     src='https://maps.google.co.id/maps?f=q&amp;source=s_q&amp;hl=en&amp;q=16+Riverside+Rd,+Singapore&amp;sll=-2.547988,118.037109&amp;sspn=29.394918,50.756836&amp;ie=UTF8&amp;geocode=Fb8IFgAdu40vBg&amp;split=0&amp;hq=&amp;hnear=16+Riverside+Rd,+Singapore&amp;ll=1.444031,103.779771&amp;spn=0.014522,0.024784&amp;t=m&amp;z=14&amp;output=embed&amp;iwloc=false'></iframe>
 
                   <address>
@@ -396,27 +260,22 @@ const Blog = () => {
                     </span>
                     <span>
                       <strong>Email:</strong>
-                      <a href='#'>
-                        contact@satriathemes.com
-                      </a>
+                      <a href='#'>contact@satriathemes.com</a>
                     </span>
                     <span>
                       <strong>Web:</strong>
-                      <a href='#'>
-                        www.satriathemes.com
-                      </a>
+                      <a href='#'>www.satriathemes.com</a>
                     </span>
                   </address>
                 </div>
 
                 <div className='widget widget-text'>
                   <h3>About Blessing</h3>
-                  Blessing is responsive website template for church build with
-                  latest Bootstrap framework. Blessing is responsive with almost
-                  all devices screen size supported. This template equipped with
-                  awesome features like Revolution Slider, touch & swipe
-                  carousel, countdown timer and much more features. Get
-                  ‘Blessing’ now!
+                  Blessing is responsive website template for church build with latest
+                  Bootstrap framework. Blessing is responsive with almost all devices
+                  screen size supported. This template equipped with awesome features like
+                  Revolution Slider, touch & swipe carousel, countdown timer and much more
+                  features. Get ‘Blessing’ now!
                 </div>
               </div>
             </div>
